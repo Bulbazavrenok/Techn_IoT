5a3f7c3ee15e34a932f9b19892a797024b5cac7b
MegaRaman <roma.rio18121811@gmail.com>
2024-03-11 12:31:38 +0100

    Complete hub part of the project

---
+++ Lab3/test.py
@@ -0,0 +1,36 @@
+# test the HTTP-endpoint to store the data
+import requests
+from app.entities.processed_agent_data import ProcessedAgentData
+from app.entities.agent_data import AgentData, AccelerometerData, GpsData
+from datetime import datetime
+from config import STORE_API_BASE_URL
+from fastapi.testclient import TestClient
+from main import app
+
+
+data = {
+    "road_state": "good",
+    "agent_data": {
+        "user_id": 5,
+        "accelerometer": {
+            "x": 4.0,
+            "y": 5.0,
+            "z": 6.0
+        },
+        "gps": {
+            "latitude": 4.05,
+            "longitude": 5.06
+        },
+        "timestamp": str(datetime.now())  # Convert to string
+    }
+}
+
+
+response = requests.post(f"http://localhost:9000/processed_agent_data", json=data)
+
+
+# Check the response
+if response.status_code == 200:
+    print("Data saved successfully!")
+else:
+    print("Failed to save data:", response.text, response.status_code)
