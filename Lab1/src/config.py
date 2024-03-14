22cc2a265dcad943e8418bbdb083b04469abe62e
Borys Kovba <borys.kovba@gmail.com>
2024-03-14 12:51:24 +0200

    refactor

--- Lab1/src/config.py
+++ Lab1/src/config.py
@@ -1,10 +1,12 @@
 import os
 
+
 def try_parse(type, value: str):
     try:
         return type(value)
     except Exception:
         return None
+
 
 # MQTT config
 MQTT_BROKER_HOST = os.environ.get('MQTT_BROKER_HOST') or 'mqtt'
@@ -12,3 +14,9 @@ MQTT_BROKER_PORT = try_parse(int, os.environ.get('MQTT_BROKER_PORT')) or 1883
 MQTT_TOPIC = os.environ.get('MQTT_TOPIC') or 'agent'
 # Delay for sending data to mqtt in seconds
 DELAY = try_parse(float, os.environ.get('DELAY')) or 1
+
+if __name__ == '__main__':
+    print("MQTT_BROKER_HOST:", MQTT_BROKER_HOST)
+    print("MQTT_BROKER_PORT:", MQTT_BROKER_PORT)
+    print("MQTT_TOPIC:", MQTT_TOPIC)
+    print("DELAY:", DELAY)
